e877d677e646dddc6b610551191d037ea5738965
Björn Kötter <dev@bk.netburn.de>
2021-11-15 15:48:45 +0100

    Code simplification and cleanup

--- cmd/check-tcp-connect.py
+++ cmd/check-tcp-connect.py
@@ -5,16 +5,16 @@ def tcp_connect(host: str, port: int):
     try:
         with socket.socket() as s:
             s.connect((host, port))
-        print('Connection to {} port {} successful'.format(host, port))
+        print(f'Connection to {host} port {port} successful')
         return 0
     except TimeoutError:
-        print('Connection to {} port {} timed out'.format(host, port))
+        print(f'Connection to {host} port {port} timed out')
         return 3
     except ConnectionRefusedError:
-        print('Connection to {} port {} refused'.format(host, port))
+        print(f'Connection to {host} port {port} refused')
         return 2
     except socket.error as e:
-        print('Connection to {} port {} failed: {}'.format(host, port, e))
+        print(f'Connection to {host} port {port} failed: {e}')
         return 2
 
 
